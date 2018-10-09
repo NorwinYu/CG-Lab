63405c2357faf680a9377890d874e2d7fb9d1e71
Norwin <norwinyu@gmail.com>
2018-10-09 13:52:33 +0800

    Lab example

---
+++ openglLabExample/cmake-build-debug/CMakeFiles/opengl.dir/DependInfo 2.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/YBH/Desktop/openglLabExample/code1.cpp" "/Users/YBH/Desktop/openglLabExample/cmake-build-debug/CMakeFiles/opengl.dir/code1.cpp.o"
+  "/Users/YBH/Desktop/openglLabExample/code2.cpp" "/Users/YBH/Desktop/openglLabExample/cmake-build-debug/CMakeFiles/opengl.dir/code2.cpp.o"
+  "/Users/YBH/Desktop/openglLabExample/main.cpp" "/Users/YBH/Desktop/openglLabExample/cmake-build-debug/CMakeFiles/opengl.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/System/Library/Frameworks/OpenGL.framework"
+  "/System/Library/Frameworks/GLUT.framework/Headers"
+  "/usr/local/Cellar/glew/2.1.0"
+  "/usr/local/Cellar/glfw/3.2.1"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ openglLabExample/cmake-build-debug/CMakeFiles/opengl.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl.dir/main.cpp.o"
+  "CMakeFiles/opengl.dir/code1.cpp.o"
+  "CMakeFiles/opengl.dir/code2.cpp.o"
+  "opengl.pdb"
+  "opengl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
